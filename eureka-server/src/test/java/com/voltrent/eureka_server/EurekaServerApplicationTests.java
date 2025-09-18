15fe372e115597a0fc6af4d31218e00b86137ee6
Kiokama <khoavl456123@gmail.com>
2025-09-18 16:37:33 +0700

    Init VoltRent Microservices Skeleton

---
+++ eureka-server/src/test/java/com/voltrent/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voltrent.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
